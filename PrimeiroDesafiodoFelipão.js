1483df39ccb4d7b6e51c4949ee3bafbe218503af
Juliana-Amaral-Silva <juliana.cmizael@gmail.com>
2024-07-16 10:59:59 -0300

    primeiro commit

---
+++ PrimeiroDesafiodoFelipão.js
@@ -0,0 +1,14 @@
+
+let nomePokemon = [
+    ["Poochyen", "M",2,13,13],
+    ["Zigzagoon", "F",2,13,13],
+    ["Dragonite", "M",5,25,25],
+    ["Dragonite", "F",5,24,24],
+    ["Poochyena", "F",3,15,15],
+    ["hurmple" ,"M",2,14,7]
+]
+console.log(nomePokemon[0][0] + " level " + nomePokemon[0][2] +
+    " Cadastrado com sucesso!! " + " Novo Registro " + nomePokemon[1][2] +
+    " leve " + nomePokemon[1][0] + " Cadastrado com sucesso!!"
+
+)
