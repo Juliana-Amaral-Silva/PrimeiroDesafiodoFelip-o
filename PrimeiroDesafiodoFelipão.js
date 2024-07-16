07a93453106bda7faacdad9c8ebff59d1d10e3e4
Juliana-Amaral-Silva <juliana.cmizael@gmail.com>
2024-07-16 11:50:54 -0300

    projeto em andamento

--- PrimeiroDesafiodoFelipão.js
+++ PrimeiroDesafiodoFelipão.js
@@ -8,7 +8,9 @@ let nomePokemon = [
     ["hurmple" ,"M",2,14,7]
 ]
 console.log(nomePokemon[0][0] + " level " + nomePokemon[0][2] +
-    " Cadastrado com sucesso!! " + " Novo Registro " + nomePokemon[1][2] +
-    " leve " + nomePokemon[1][0] + " Cadastrado com sucesso!!"
+    " Cadastrado com sucesso!! " + "Novo Registro " + nomePokemon[1][0] +
+    " level " + nomePokemon[1][2] + 
+    " Cadastrado com sucesso!!" + " Novo Registro " + nomePokemon[2][0] +
+    " level " + nomePokemon[3][2] + " Cadastrado com sucesso!!"
 
 )
---
+++ README.md
@@ -0,0 +1,10 @@
+O QUE PRECISA SER FEITO ?
+
+Crie um código para armazenar o máximo de informações possível dos pokemon, para cada um, exiba uma mensagem de saída escrita o nome do pokemon concatenado com "Cadastrado com sucesso"
+
+Poochyena  - M - lv2 - 13 vid ini - 13 vida fin
+Zigzagoon  - F - lv2 - 13 vid ini - 13 vida fin
+Dragonite  - M - lv5 - 25 vid ini - 25 vida fin
+Dragonite  - F - lv5 - 24 vid ini - 24 vida fin
+Poochyena  - F - lv3 - 15 vid ini - 15 vida fin
+hurmple    - M - lv2 - 14 vid ini - 7 vida  fin
